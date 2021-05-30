32ec1b409e1e9d8fdea44b8aef09b2b4703ddc04
Dustin <dustindoctorz@gmail.com>
2021-05-30 12:39:32 -0400

    finished styling, began javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const display = document.getElementById('display');
+var displayVal = 0;
+
+function main(){
+    display.textContent = displayVal;
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*': 
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
+
+main();
